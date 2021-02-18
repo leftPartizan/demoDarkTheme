371de69f617a68c885e801ee3044fc84ac612d01
vladlen <white8pilot@gmail.com>
2021-02-18 19:25:22 +0300

    first commit

---
+++ app/src/main/java/com/example/darkthemedemo/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.darkthemedemo
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.darkthemedemo.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initThemeState()
+        addChangeThemeListener()
+        setContentView(binding.root)
+        setSupportActionBar(binding.appBarMain.toolbar)
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun addChangeThemeListener() {
+        val image = binding.navView.getHeaderView(0).findViewById<ImageView>(R.id.theme_button)
+        image.setOnClickListener {
+            val mPrefs = getSharedPreferences("THEME", MODE_PRIVATE)
+            val isDarkTheme = mPrefs.getBoolean("theme_boolean", false)
+            if (isDarkTheme) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            val mEditor = mPrefs.edit()
+            mEditor.putBoolean("theme_boolean", !isDarkTheme)
+            mEditor.apply()
+        }
+    }
+
+    fun initThemeState() {
+        val mPrefs = getSharedPreferences("THEME", MODE_PRIVATE)
+        val isDarkTheme = mPrefs.getBoolean("theme_boolean", true)
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+}
